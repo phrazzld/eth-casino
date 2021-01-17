8377530e0a11c92ef118cd10af0cbdd5f615564a
Phaedrus Raznikov <praznikov@cerego.com>
2021-01-16 19:46:20 -0500

    Init to win it

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// const path = require("path")
+import path from "path";
+
+module.exports = {
+  entry: path.join(__dirname, "src/js", "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$$/,
+        use: ["style-loader", "css-loader"],
+        include: /src/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015", "react", "stage-2"]
+        }
+      }
+    ]
+  }
+};
